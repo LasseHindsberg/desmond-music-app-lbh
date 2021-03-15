41a29f60369685e299ffd4d6af961393b6dd0dbd
LasseHindsberg <59876303+FrostedHazel@users.noreply.github.com>
2021-03-15 14:54:32 +0100

    small fix

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,7 @@
 export default function Album({src}) {
     return (
         <>
+        {/* eslint-disable-next-line */}
         <iframe src={src} width="400" height="400" frameborder="0" allow="encrypted-media"></iframe>
         </>
     )
--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -4,6 +4,7 @@ export default function Copyright() {
         <h4>Project made by Lasse Bruun Hindsberg</h4>
         <a href="https://github.com/LasseHindsberg" className="github">Github Profile</a>
         <br/>
+        {/* eslint-disable-next-line */}
         <iframe src="https://open.spotify.com/follow/1/?uri=spotify:user:1137034155&size=detail&theme=dark&show-count=0" width="300" height="56" scrolling="no" frameborder="0" allowtransparency="true"></iframe>
         </section>
     )
